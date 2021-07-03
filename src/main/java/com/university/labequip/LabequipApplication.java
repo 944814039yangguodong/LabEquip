bd57d94c3cbf284a995750568519c4ee503d8fc1
yangguodong944814039 <944814039@qq.com>
2021-07-04 01:45:24 +0800

    初始化

--- src/main/java/com/university/labequip/LabequipApplication.java
+++ src/main/java/com/university/labequip/LabequipApplication.java
@@ -1,9 +1,11 @@
 package com.university.labequip;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan
 public class LabequipApplication {
 
 	public static void main(String[] args) {
